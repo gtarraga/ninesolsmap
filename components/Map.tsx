1290b9f221ba9b68a36751e7bd6faf82d15c83f8
gtarraga <gt.tarraga@gmail.com>
2024-06-13 00:16:45 +0200

    Debugging in prod

--- components/Map.tsx
+++ components/Map.tsx
@@ -71,7 +71,10 @@ const MapComponent: React.FC<MapComponentProps> = ({ markerId }) => {
 
   // Open popup if a single marker is shown
   const markerInit = (ref: L.Marker) => {
+    // logging for production
+    console.log('a');
     if(ref) {
+      console.log('ref', ref);
       ref.openPopup()
     }
   }
