131f0354d725770accd653e4ae4c7a920d2a7361
gtarraga <gt.tarraga@gmail.com>
2024-06-12 02:10:13 +0200

    Test og 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,20 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Nine Sols Interactive Map",
   description: "To save you time finding that last item in an area.",
-  openGraph: {
-    title: 'Interactive Map Graphic',
-    description: 'A Nine Sols interactive map',
-    url: 'https://ninesolsmap.com',
-    siteName: 'Nine Sols Interactive Map',
-    images: [
-      {
-        url: './opengraph-image.png',
-        height: 636,
-      },
-    ],
-    locale: 'en_US',
-    type: 'website',
-  },
+  metadataBase: new URL('https://ninesolsmap.com'),
 };
 
 export default function RootLayout({
